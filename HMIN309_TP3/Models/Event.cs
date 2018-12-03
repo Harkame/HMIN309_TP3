bd4984b7b506da76a7b9bdc4ee525d4335a07c89
Daviaud Louis <louis-richard.daviaud@etu.umontpellier.fr>
2018-12-03 12:12:06 +0100

    some dev

---
+++ HMIN309_TP3/Models/Event.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+namespace HMIN309_TP3.Models
+{
+    [Table("Events")]
+    public class Event
+    {
+        [PrimaryKey, AutoIncrement, Column("id")]
+        public int Id { get; set; }
+
+        [NotNull, Column("name")]
+        public string Name { get; set; }
+
+        [NotNull, Column("date")]
+        public long Date { get; set; }
+
+        [NotNull, Column("type")]
+        public string Type { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+    }
+}
---
+++ HMIN309_TP3/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HMIN309_TP3.Models;
+
+namespace HMIN309_TP3.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Event Item { get; set; }
+        public ItemDetailViewModel(Event item = null)
+        {
+            Title = item?.Name;
+            Item = item;
+        }
+    }
+}
